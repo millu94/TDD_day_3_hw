59abeea3363791e3bbb9dbdc60df5500c5062df9
Joshua <jssmiller94@gmail.com>
2021-04-07 19:52:37 +0100

    improved compare() function to pass tests

--- src/compare.py
+++ src/compare.py
@@ -4,4 +4,6 @@ def compare(num_1, num_2):
     if num_1 > num_2:
         return f"{num_1} is greater than {num_2}"
     if num_2 > num_1:
-        return "{num_2} is greater than {num_3}"
+        return f"{num_2} is greater than {num_3}"
+    if num_1 == num_2:
+        return f"{num_1} is equal to {num_2}"
